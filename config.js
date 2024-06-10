7824503fccf0f9110fc7bb7a5643078e5598788f
divrawat <divrawat2001@gmail>
2024-06-10 14:49:06 +0530

    s0efs5eg

--- config.js
+++ config.js
@@ -2,8 +2,19 @@
 // export const DOMAIN = 'http://localhost:3000';
 export const DOMAIN = 'https://vinland-saga-eight.vercel.app';
 
+
+export const MANGA_DESCRIPTION = `Set in the Viking era, this manga follows Thorfinn's quest for revenge and his dream of finding the legendary land of Vinland. Filled with action and historical drama, <a href="${DOMAIN}"><u>Vinland Saga</u></a> is a thrilling story about battles, honor, and the search for peace. Perfect for fans of exciting, historical tales. Experience the gripping adventures of Thorfinn as he navigates through the tumultuous Viking world, facing formidable challenges and discovering his own path to redemption.`;
+
 export const MANGA_NAME = 'Vinland Saga';
-export const APP_DESCRIPTION = 'Read Vinland Saga manga online at Vinland Saga Haven. Enjoy high-quality scans, latest chapters, and connect with fans in our vibrant community. Dive into the epic Viking adventure today!';
+export const MANGA_AUTHOR = 'YUKIMURA Makoto';
+export const MANGA_RELEASE = '2015';
+export const MANGA_STATUS = 'Ongoing';
+export const MANGA_ARTIST = 'Makoto YUKIMURA ';
+export const MANGA_STUDIO = 'Wit, MAPPA';
+export const MANGA_GENRE = 'Action, Adventure, Drama, Historical, Seinen';
+
+
+export const APP_DESCRIPTION = `Read Vinland Saga manga online at "${DOMAIN}". Enjoy high-quality scans, latest chapters, and connect with fans in our vibrant community. Dive into the epic Viking adventure today!`;
 
 // Navbar
 export const logo = <img src={`${DOMAIN}/logo.jpg`} alt="Logo" className="h-[75px] w-[185px] mr-5 md:ml-0 ml-4 md:pb-2 md:my-0 my-1" />
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,8 @@
-import { DOMAIN } from "@/config";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import Image from "next/image";
+import Link from "next/link";
+import { DOMAIN, MANGA_NAME, MANGA_DESCRIPTION, MANGA_AUTHOR, MANGA_RELEASE, MANGA_STATUS, MANGA_ARTIST, MANGA_STUDIO, MANGA_GENRE } from "@/config";
 
 export default function Home() {
   const chapters = Array.from({ length: 211 }, (_, i) => ({
@@ -8,23 +10,85 @@ export default function Home() {
     url: `${DOMAIN}/manga/vinland-saga-chapter-${i + 1}`
   })).reverse();
 
+  const genres = MANGA_GENRE.split(', ');
+
   return (
     <>
       <Navbar />
+      <article>
+        <div className="flex md:mb-[60px] mb-5 pt-3 bg-[#000000] text-[white]">
+          <div className="w-2/5"><Image width={450} height={450} src={`${DOMAIN}/cover.webp`}></Image></div>
+          <div className="w-3/5 md:mr-10">
+            <h1 className="text-center font-extrabold text-4xl my-5 uppercase">{MANGA_NAME}</h1>
+            <p className="my-5 leading-[2] text-center" dangerouslySetInnerHTML={{ __html: MANGA_DESCRIPTION }} />
 
-      <div className="mt-10 max-w-[1100px] mx-auto px-5 flex flex-wrap justify-center">
+            <div className="flex flex-wrap justify-center gap-2">
+              {genres.map((genre, index) => (
+                <button key={index} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                  {genre}
+                </button>
+              ))}
+            </div>
 
-        {chapters.map((chapter) => (
-          <div className="flex hover:scale-105 active:scale-95 transition-transform" key={chapter.number}>
-            <a href={chapter.url} className="p-5">
-              <p className="w-[300px] text-center p-5 border border-l-8 border-[black] font-bold">
-                Vinland Saga, Chapter {chapter.number}
-              </p>
-            </a>
+
+
+
+
+            <div className="grid grid-cols-3 justify-center gap-x-10 mt-5">
+              <div className="text-center mt-10">
+                <h3 className="font-bold text-[21px] mb-2">Release</h3>
+                <p>{MANGA_RELEASE}</p>
+              </div>
+              <div className="text-center mt-10">
+                <h3 className="font-bold text-[21px] mb-2">Status</h3>
+                <p>{MANGA_STATUS}</p>
+              </div>
+              <div className="text-center mt-10">
+                <h3 className="font-bold text-[21px] mb-2">Author</h3>
+                <p>{MANGA_AUTHOR}</p>
+              </div>
+              <div className="text-center mt-10">
+                <h3 className="font-bold text-[21px] mb-2">Type</h3>
+                <p>Manga</p>
+              </div>
+              <div className="text-center mt-10">
+                <h3 className="font-bold text-[21px] mb-2">Artist</h3>
+                <p>{MANGA_ARTIST}</p>
+              </div>
+              <div className="text-center mt-10">
+                <h3 className="font-bold text-[21px] mb-2">Studio</h3>
+                <p>{MANGA_STUDIO}</p>
+              </div>
+            </div>
+
+            <div className="flex justify-center mt-10">
+              <button className="bg-blue-500 hover:scale-110 active:scale-95 transition-transform text-white font-bold py-2 px-4 rounded">
+                <a href="#readmanga">  READ NOW</a>
+              </button>
+            </div>
+
+
+
           </div>
-        ))}
-      </div>
+        </div>
 
+
+        <h2 id="readmanga" className="font-extrabold text-3xl my-10 text-center">Read {MANGA_NAME} Chapters Online</h2>
+
+        <div className="mt-10 max-w-[1100px] mx-auto px-5 flex flex-wrap justify-center">
+
+          {chapters.map((chapter) => (
+            <div className="flex hover:scale-105 active:scale-95 transition-transform" key={chapter.number}>
+              <Link href={chapter.url} className="p-5">
+                <p className="w-[300px] text-center p-5 border border-l-8 border-[black] font-bold break-words">
+                  Vinland Saga, Chapter {chapter.number}
+                </p>
+              </Link>
+            </div>
+          ))}
+
+        </div>
+      </article>
       <Footer />
     </>
   );
